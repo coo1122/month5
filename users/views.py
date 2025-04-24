a2cc3622b9fd10deca99aa71b3bdacc605587c1c
coo1122 <adiletusenov50@gmail.com>
2025-04-24 00:20:21 -0700

    Lesson 6

--- users/views.py
+++ users/views.py
@@ -5,19 +5,20 @@ from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializer, UserAuthSerializer
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import APIView
 
-@api_view(['GET'])
-def authorization_api_view(request):
-    serializer = UserAuthSerializer(request.user)
-    serializer.is_valid(raise_exception=True)
+class AuthAPIView(APIView):
+    def post(self, request):
+        serializer = UserAuthSerializer(request.user)
+        serializer.is_valid(raise_exception=True)
 
-    user = authenticate(**serializer.validated_data)
+        user = authenticate(**serializer.validated_data)
 
-    if user is not None:
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
-    return Response(status=status.HTTP_401_UNAUTHORIZED,
-                    data={'error': 'User credential are wrong!'})
+        if user is not None:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response(status=status.HTTP_401_UNAUTHORIZED,
+                        data={'error': 'User credential are wrong!'})
 
 @api_view(['POST'])
 def registration_api_view(request):
